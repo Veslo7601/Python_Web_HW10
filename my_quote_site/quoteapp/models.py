5c004fef6e75c3cb77ad06e859f2e69f60e1ab05
Veslo7601 <igrok7601@gmail.com>
2024-05-22 23:42:28 +0300

    deleted:    detail.html 	new file:   my_quote_site/authors.json 	new file:   my_quote_site/qoutes.json 	modified:   my_quote_site/quoteapp/__pycache__/models.cpython-312.pyc 	modified:   my_quote_site/quoteapp/__pycache__/views.cpython-312.pyc 	new file:   my_quote_site/quoteapp/management/commands/__pycache__/import_data.cpython-312.pyc 	new file:   my_quote_site/quoteapp/management/commands/__pycache__/load_data.cpython-312.pyc 	new file:   my_quote_site/quoteapp/management/commands/import_data.py 	new file:   my_quote_site/quoteapp/migrations/0002_alter_autors_born_date_alter_autors_born_location_and_more.py 	new file:   my_quote_site/quoteapp/migrations/0003_alter_autors_description_alter_quotes_quote_and_more.py 	new file:   my_quote_site/quoteapp/migrations/0004_alter_autors_description_alter_quotes_quote.py 	new file:   my_quote_site/quoteapp/migrations/__pycache__/0002_alter_autors_born_date_alter_autors_born_location_and_more.cpython-312.pyc 	new file:   my_quote_site/quoteapp/migrations/__pycache__/0003_alter_autors_description_alter_quotes_quote_and_more.cpython-312.pyc 	new file:   my_quote_site/quoteapp/migrations/__pycache__/0004_alter_autors_description_alter_quotes_quote.cpython-312.pyc 	modified:   my_quote_site/quoteapp/models.py 	deleted:    my_quote_site/quoteapp/templates/quoteapp/index copy.html 	modified:   my_quote_site/quoteapp/views.py

--- my_quote_site/quoteapp/models.py
+++ my_quote_site/quoteapp/models.py
@@ -3,16 +3,16 @@ from django.db import models
 # Create your models here.
 
 class Autors(models.Model):
-    name = models.CharField(max_length=32, null=False, unique=True)
-    born_date = models.CharField(max_length=32, null=False)
-    born_location = models.CharField(max_length=64, null=False)
-    description = models.CharField(max_length=256, null=False)
+    name = models.CharField(max_length=64, null=False, unique=True)
+    born_date = models.CharField(max_length=128, null=False)
+    born_location = models.CharField(max_length=128, null=False)
+    description = models.TextField(null=False)
 
     def __str__(self):
         return f"{self.name}"
 
 class Tag(models.Model):
-    name = models.CharField(max_length=25, null=False, unique=True)
+    name = models.CharField(max_length=64, null=False, unique=True)
 
     def __str__(self):
         return f"{self.name}"
@@ -20,4 +20,4 @@ class Tag(models.Model):
 class Quotes(models.Model):
     tags =  models.ManyToManyField(Tag)
     author = models.ForeignKey(Autors, on_delete=models.CASCADE)
-    quote = models.CharField(max_length=126, null=False)
+    quote = models.TextField(null=False)
