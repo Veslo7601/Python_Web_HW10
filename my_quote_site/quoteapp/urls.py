344df214659c60ce8f1443e7aca7f629d37c712e
Veslo7601 <igrok7601@gmail.com>
2024-05-22 21:05:50 +0300

    modified:   my_quote_site/my_quote_site/__pycache__/settings.cpython-312.pyc 	modified:   my_quote_site/my_quote_site/__pycache__/urls.cpython-312.pyc 	modified:   my_quote_site/my_quote_site/settings.py 	modified:   my_quote_site/my_quote_site/urls.py 	modified:   my_quote_site/quoteapp/__pycache__/urls.cpython-312.pyc 	modified:   my_quote_site/quoteapp/__pycache__/views.cpython-312.pyc 	new file:   my_quote_site/quoteapp/templates/quoteapp/author_detail.html 	new file:   my_quote_site/quoteapp/templates/quoteapp/index copy.html 	modified:   my_quote_site/quoteapp/templates/quoteapp/index.html 	modified:   my_quote_site/quoteapp/urls.py 	modified:   my_quote_site/quoteapp/views.py 	new file:   my_quote_site/users/__init__.py 	new file:   my_quote_site/users/__pycache__/__init__.cpython-312.pyc 	new file:   my_quote_site/users/__pycache__/admin.cpython-312.pyc 	new file:   my_quote_site/users/__pycache__/apps.cpython-312.pyc 	new file:   my_quote_site/users/__pycache__/forms.cpython-312.pyc 	new file:   my_quote_site/users/__pycache__/models.cpython-312.pyc 	new file:   my_quote_site/users/__pycache__/urls.cpython-312.pyc 	new file:   my_quote_site/users/__pycache__/views.cpython-312.pyc 	new file:   my_quote_site/users/admin.py 	new file:   my_quote_site/users/apps.py 	new file:   my_quote_site/users/forms.py 	new file:   my_quote_site/users/migrations/__init__.py 	new file:   my_quote_site/users/migrations/__pycache__/__init__.cpython-312.pyc 	new file:   my_quote_site/users/models.py 	new file:   my_quote_site/users/templates/users/login.html 	new file:   my_quote_site/users/templates/users/signup.html 	new file:   my_quote_site/users/tests.py 	new file:   my_quote_site/users/urls.py 	new file:   my_quote_site/users/views.py

--- my_quote_site/quoteapp/urls.py
+++ my_quote_site/quoteapp/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('tag/', views.tag_create, name='tag'),
     path('author/', views.author_create, name='author'),
     path('quote/', views.quote_create, name='quote'),
+    path('author_detail/<int:author_id>', views.author_detail, name='author_detail'),
 ]
--- my_quote_site/quoteapp/views.py
+++ my_quote_site/quoteapp/views.py
@@ -1,14 +1,19 @@
-from django.shortcuts import render, redirect
-from .models import Tag, Quotes, Autors
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Quotes, Autors
 from .forms import TagForm, AuthorForm, QuoteForm
+from django.core.paginator import Paginator
 
 
 # Create your views here.
 
 
 def main(request):
-    quote_o = Quotes.objects.all()
-    return render(request, 'quoteapp/index.html', {"quote": quote_o})
+    quotes_list = Quotes.objects.all()
+    paginator = Paginator(quotes_list, 10)
+
+    page_number = request.GET.get('page')
+    quotes = paginator.get_page(page_number)
+    return render(request, 'quoteapp/index.html', {'quotes': quotes})
 
 def tag_create(request):
     if request.method == 'POST':
@@ -39,3 +44,7 @@ def quote_create(request):
     else:
         form = QuoteForm()
     return render(request, 'quoteapp/quote_form.html', {'form': form})
+
+def author_detail(request, author_id):
+    author = get_object_or_404(Autors, pk=author_id)
+    return render(request, 'quoteapp/author_detail.html', {"autors": author})
